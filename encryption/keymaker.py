2f6f8277f708aef5c11c03687ecdfc4f9ec64e6b
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2020-07-09 17:02:14 +0300

    fast commit

--- encryption/keymaker.py
+++ encryption/keymaker.py
@@ -1,10 +1,12 @@
 from Crypto.PublicKey import RSA
 
+
 def generate_key(path):
     key = RSA.generate(4096)
     f = open(f'{path}/key.pem', 'wb')
     f.write(key.exportKey('PEM'))
     f.close()
+
 
 # #Read key from file
 # f = open('keyfile.pem', 'rb')
