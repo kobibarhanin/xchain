c33a7d37c6fe20ca8d2360c82009f6e287a2f60a
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2020-07-09 17:05:21 +0300

    fast commit

--- tests/test_keys_config.py
+++ tests/test_keys_config.py
@@ -1,5 +1,5 @@
 
 ENC_KEY = b'1234567890123456'
 
-SIG_KEY_PATH = 'test_keys/key.pem'
-VER_KEY_PATH = 'test_keys/key.pem'
+SIG_KEY_PATH = 'tests/test_keys/key.pem'
+VER_KEY_PATH = 'tests/test_keys/key.pem'
